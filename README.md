f4f77e76a3d7a1b7d96439470869eef8f050845b
Avijit Karmoker <ovijit1hsc@gmail.com>
2021-03-11 22:29:52 +0600

    all done

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 There I make a all leagues where in home page you can see the league name, sports type beside this if you want to see the club details then you can click on explore and see the details.
 
-Here the live site: 
+Here the live site: https://angry-dijkstra-9e1277.netlify.app
--- src/components/Leagues/Leagues.js
+++ src/components/Leagues/Leagues.js
@@ -4,12 +4,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import "./Leagues.css";
+import { useEffect, useState } from "react";
 
 const Leagues = (props) => {
   let { strLeagueAlternate, strSport, idLeague } = props.league;
+
+  const [leagueLogo, setLeagueLogo] = useState([]);
+
+  useEffect(() =>{
+    const url = `https://www.thesportsdb.com/api/v1/json/1/lookupleague.php?id=${idLeague}`;
+    fetch(url)
+    .then((response) => response.json())
+    .then(data => setLeagueLogo(data.leagues))
+  }, [idLeague])
   return (
     <div className="cart">
           <div className="leagues">
+            {
+              leagueLogo?.map(logo => <img src={logo.strLogo} alt="lOGO"/>)
+            }
             <h4>{strLeagueAlternate}</h4>
             <p>Sports type: {strSport}</p>
             <Link to={`${idLeague}`} style={{ color: "#fff" }}>
